c2a9304a9da793b18c35d171fb37ae1a4563916a
jana prasanna <emailtojana27@gmail.com>
2021-04-19 19:21:57 +0530

    ternary updated

---
+++ vit_sem2_cprojects/Day codes/ternary.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char string[50];
+    int a,b,result=0;
+    for(a=0;a<=5;a++)
+    {
+        printf("Happy birthday %s\n",a==4?"dear rohan":"to you");
+        
+    }
+    // program 2
+    a=5;b=6;
+    printf("the given number is %s",a%2==0?"even":"odd");
+    printf("")
+}
