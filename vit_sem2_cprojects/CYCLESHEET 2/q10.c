408061ea52248da9b10bd1cf63f76aecd78ba309
jana prasanna <emailtojana27@gmail.com>
2021-04-23 12:10:06 +0530

    cyclesheet 2 q10

--- vit_sem2_cprojects/CYCLESHEET 2/q10.c
+++ vit_sem2_cprojects/CYCLESHEET 2/q10.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 void main()
 {
-    
+    int arr[5],position=0,replace;
+    printf("enter 5 values to the array: \n");
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("the array elements are : ");
+    for(int i=0;i<5;i++)
+        printf("%d  ",arr[i]);
+    printf("enter the posion of the value to be deleted : ");
+    scanf("%d",&position);
+    position = position -1;
+    printf("\nenter the replacement value to be inserted at the deleted location : ");
+    scanf("%d",&replace);
+    arr[position] = replace;
+    printf("after inserting.....\n");
+    for(int i=0;i<5;i++)
+        printf("%d  ",arr[i]);
 }
