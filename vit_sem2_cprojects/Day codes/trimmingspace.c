d8d9ec2f7120ae73c4b47014a3a97c96bc57a9dd
jana prasanna <emailtojana27@gmail.com>
2021-04-27 17:26:09 +0530

    string pgms

--- vit_sem2_cprojects/Day codes/trimmingspace.c
+++ vit_sem2_cprojects/Day codes/trimmingspace.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-void main()
+int main()
 {
     char s[1000];
     int i,j;
-    printf("Enter any string: ");
+    printf("Enter any string:");
     gets(s);
-    printf("Before trimming : %s",s);
+    printf("Before trimming :%s",s);
     
-    for(i=0;s[i]==' '||s[i]=='\t';i++);
+    for(i=0;s[i]==' '||s[i]=='\t';i++); // counts the space in leading postion (not in trailing)
+        printf("\n%d",i);
 
     for(j=0;s[i];i++)
     {
@@ -17,8 +18,10 @@ void main()
     for(i=0;s[i];i++)
     {
         if(s[i]!=' '&&s[i]!='\t')
-            j=i;
+            printf("\n%d",j);
+            j=1;  // i is copied to j so that j+1 reaches the end of string with no spaces.
     }
-    s[j+1]='\0';
-    printf("\nAfter trimming : %s",s);
+    s[j+1]='\0'; //terminating the index before the trailing space
+    printf("\nAfter trimming :%s",s);
+    return 0;
 }
