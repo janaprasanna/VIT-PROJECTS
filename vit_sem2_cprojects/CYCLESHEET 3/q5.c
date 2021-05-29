92989bcb74819fae0d2f9515bb96d4cdf87bb094
jana prasanna <emailtojana27@gmail.com>
2021-05-29 16:41:43 +0530

    q5

--- vit_sem2_cprojects/CYCLESHEET 3/q5.c
+++ vit_sem2_cprojects/CYCLESHEET 3/q5.c
@@ -44,15 +44,14 @@ char strrep(char str[],char word[],char replace[],int s_space,int e_space)
     dummytext[j]='\0';
   
     printf("\ndummytext=%s",dummytext);  //checking if the words are properly copied.
-    /*if(strcmp(dummytext,word)==0)
+    if(strcmp(dummytext,word)==0)
     { 
-    
-            for(j=0,i=s_space;i<e_space;j++,i++)
+            for(j=0,i=s_space;i<=e_space;j++,i++)
             {
                 str[i]= replace[j];
             }
-            
-    }*/
-    //printf("\n\nmodifed:%s",str);  //checking
+    }
+    printf("\n\nmodifed:%s",str);  //checking
+    return *str;
 
 }
