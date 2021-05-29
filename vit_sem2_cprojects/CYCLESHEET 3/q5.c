085e27a8a1eb1c6a5a9a5f9b34ded6b38ea8e20d
jana prasanna <emailtojana27@gmail.com>
2021-05-29 13:08:38 +0530

    q5

--- vit_sem2_cprojects/CYCLESHEET 3/q5.c
+++ vit_sem2_cprojects/CYCLESHEET 3/q5.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<string.h>
-void strrep(char*,char*,char *,int ,int );
+char strrep(char*,char *,char *,int ,int );
 void main()
 {
-    char str[100],word[10],replace[10];int i,e_space=0,s_space=0;
+    char str[100],word[10],replace[10];int i,e_space=0,s_space=0;//e_space = ending space; s_space=starting space
+    int temp=0;
     printf("enter a string:");
     gets(str);
     printf("enter the word that needs to be removed from the string:");
@@ -14,37 +15,40 @@ void main()
 
     for(i=0;i<strlen(str);i++)  // 0 -  9
     {
-       
         if(str[i]==' ')   //i=5 has " "
         {
-            
-            e_space=i;
-            
-            strrep(&str,word,replace,s_space,e_space);
+            e_space=i;  
+            temp=e_space; 
+            strrep(str,word,replace,s_space,e_space); // 0 - 5
             s_space=e_space+1;
+            printf("\ns_space=%d",s_space);
         }
-        
-   
+        else if(str[i]=='\0')
+        {
+            e_space=strlen(str);
+            strrep(str,word,replace,s_space,e_space);
+         
+        }
     }
 }
-void strrep(char *str,char word[],char replace[],int s_space,int e_space)
+char strrep(char str[],char word[],char replace[],int s_space,int e_space)
 {
-    char dummytext[10]="";int j=0,i;
-    for(int i=s_space,j=0 ;i<e_space;i++,j++)
+    char dummytext[10];int j=0,i;
+    for(i=s_space,j=0 ;i<e_space;i++,j++)
     {
-            dummytext[j]= *(str+i);
+            dummytext[j]= str[i];
     }
-    //printf("\ndummytext= %s",dummytext);  //checking if the words are properly copied.
-    if(strcmp(dummytext,word)==0)
+    printf("\ne_space=%d",e_space);
+    printf("\ndummytext= %s",dummytext);  //checking if the words are properly copied.
+    /*if(strcmp(dummytext,word)==0)
     { 
     
             for(j=0,i=s_space;i<e_space;j++,i++)
             {
-                *(str+i)=replace[j];
+                str[i]= replace[j];
             }
             
-        
-    }
-    printf("\n\nmodifed:%s",str);  //checking
+    }*/
+    //printf("\n\nmodifed:%s",str);  //checking
 
 }
