a6796958e0c0ddb12f156c0942bcc27f09c17d80
jana prasanna <emailtojana27@gmail.com>
2021-05-30 12:29:33 +0530

    q4

--- vit_sem2_cprojects/CYCLESHEET 3/q4.c
+++ vit_sem2_cprojects/CYCLESHEET 3/q4.c
@@ -9,8 +9,19 @@ void main()
     printf("enter 'n': ");
     scanf("%d",&n);
     
-    Y=pow(x,n);
+    //Y=pow(x,n);
 
-    printf("Y=%f",Y);
     
+    if(n<0)
+    {
+        n= -n;
+        for(int i=1;i<=n;i++)
+            Y=Y*(1/x);
+    }
+    else    
+    {
+        for(int i=1;i<=n;i++)
+            Y=Y*x;
+    }
+    printf("Y=%f",Y);
 }
