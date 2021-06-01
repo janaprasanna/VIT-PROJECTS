d73e93288d91a2ba2305d1ec838f968d68799fcf
jana prasanna <emailtojana27@gmail.com>
2021-06-01 15:21:05 +0530

    q5

--- vit_sem2_cprojects/Day codes/cc3q5.c
+++ vit_sem2_cprojects/Day codes/cc3q5.c
@@ -1,67 +1,47 @@
-#include<stdio.h>
-#include<string.h>
-void strreplace(char*,char *,char *);
-void main()
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+void stringReplace(const char *str, const char *subStr, const char *replaceStr)
 {
-    char str[100],word[10],replace[10];
-
-    printf("enter a string:");
-    gets(str);
-    printf("enter the word that needs to be removed from the string:");
-    gets(word);
-    printf("enter the word that should be replaced within that position:");
-    gets(replace);
-    strreplace(str,word,replace);
-    printf("\nafter replacing:%s",str);
+    int i = 0, count = 0;
+    int len1 = strlen(replaceStr);
+    int len2 = strlen(subStr);
+ 
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (strstr(&str[i], subStr) == &str[i])
+        {
+            count++;
+            i += len2 - 1;
+        }
+    }
+    char *newString = (char *)malloc(i + count * (len1 - len2) + 1);
+    i = 0;
+    while (*str)
+    {
+        if (strstr(str, subStr) == str)
+        {
+            strcpy(&newString[i], replaceStr);
+            i += len1;
+            str += len2;
+        }
+        else
+            newString[i++] = *str++;
+    }
+    newString[i] = '\0';
+ 
+    printf("New String: %s\n", newString);
 }
-void strreplace(char str[],char word[],char replace[])
+int main()
 {
-    int lenstr,lenword,lenreplace,i=0,j,k,start,end;char temp[50];
-    lenstr=strlen(str);
-    lenword=strlen(word);
-    lenreplace=strlen(replace);
-    strcpy(temp,str);
-    for(i=0;i<lenstr;i++)
-    {
-        for(j=0;str[i]==word[j];j++)   //hhello            hello
-        {
-            if(str[i]==word[j])
-                start=i;
-            else    
-            {
-                start=start-lenword-1;
-                printf("start=%d",start);
-                break;
-            }   
-            printf("\nstart=%d",start);
-        }
-        for(j=0;j<lenstr;j++)
-        {
-            if(str[j]==' ')
-            {
-                end=j;
-                break;
-            }
-            if(str[j]=='\0')
-            {
-                end=j;
-                break;
-            }
-            printf("\nend=%d",end);
-        }
-    }
-    
-    //printf("\nstart=%d\nend=%d\n",start,end);
-    for(j=0,k=start;j<lenreplace;j++,k++)
-    {
-        str[k]=replace[j];
-    }
-
-    str[k]=' ';
-    for(;temp[end];)
-    {
-        str[++k]=temp[++end];
-    }
-    //edited
-    
+    printf("\n\n");
+    char str[] = "There are 30 bananas on a banana tree";
+    char subStr[] = "banana";
+    char replaceStr[] = "apple";
+ 
+    printf("Original string: %s\n", str);
+    stringReplace(str, subStr, replaceStr);
+    printf("\n\n");
+    return 0;
 }
