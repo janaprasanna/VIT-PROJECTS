b722de0a118037ec2ded9596350f476e826dcadf
jana prasanna <emailtojana27@gmail.com>
2021-06-06 20:22:07 +0530

    techgig

--- vit_sem2_cprojects/comp_code2.c
+++ vit_sem2_cprojects/comp_code2.c
@@ -1,51 +1,57 @@
 #include<stdio.h>
 void main()
 {
-    int low,high,i,j,result,x,flag,temp1,temp2;
+    int low,i,j,result,flag,temp1,temp2,n,x=0,c=0;
     char choice;
-    printf("enter range:");
-    scanf("%d",&high);
-    printf("\ndo you want to enter lower bound?(y/n) ");
+    printf("Enter the range till which you want prime numbers\n");
+    scanf("%d",&n);
+    printf("do you want to enter lower bound?(y/n) : ");
     fflush(stdin);
     scanf("%c",&choice);
     if(choice=='y')
-        {
-            printf("enter lower bound:");
-            scanf("%d",&low);
-        }
+    {
+        printf("enter lower bound:");
+     
+        scanf("%d",&low);
+    }
     else
         low=0;
-    int arr[high];
-     for(i=low,x=0; i<=high; i++,x++)
-   {
-      flag=0;
-      for(j=2; j<=i/2; j++)
-      {
-         if(i%j==0)
-         {
-            flag=1;
-            break;
-         }
-      }
-      if(flag==0)
-         arr[x]=i;
-      else
-        arr[x]=0;
-   }
-   for(i=0;i<x;i++)
-        printf("%d  ",arr[i]);
-    temp2=arr[0];temp1=arr[0];
-    for(i=0;i<=x;i++)
+        
+    int arr[n-low];
+    for(i=low,x=0;i<=n;i++)
     {
-        for(j=1;j<=x-1;j++)
+        int flag=0;
+        for(j=1;j<=i;j++)
         {
-            if(temp1<arr[j])
-                temp1=arr[i];
-            if(temp2>arr[j]&&arr[j]!=0)
-                temp2=arr[j];
+            if(i%j==0)
+            {
+                flag++;
+            }
+        }
 
+       if(flag==2)
+        {
+           arr[x]=i;
+           x++;
+           c++;
         }
     }
+    for(i=0;i<c;i++)
+        printf("%d  ",arr[i]);
+
+    temp1=arr[0];//smaller
+    temp2=arr[0];//larger
+    
+    for(i=0;i<c;i++)
+    {
+        if(temp1>arr[i])
+        {
+            temp1=arr[i];
+        }
+        if(temp2<arr[i])
+            temp2=arr[i];
+    }
     printf("\ntemp1=%d\ntemp2=%d",temp1,temp2);
-    printf("\ndifference between maximum prime nos: %d",temp1-temp2);
+    printf("\ndifference between max prime nos: %d",temp2-temp1);
+
 }
