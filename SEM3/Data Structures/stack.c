9a110a4e01bf1e20d48db211d6b6ac053b5419fc
jana prasanna <emailtojana27@gmail.com>
2021-09-01 15:50:46 +0530

    stack using arrays

--- SEM3/Data Structures/stack.c
+++ SEM3/Data Structures/stack.c
@@ -1,66 +1,80 @@
 #include<stdio.h>
-#define SIZE 3
-void push(int [], int, int *);
-void pop(int [], int *);
-void main()
+#include <stdlib.h>
+
+void push(int *, int, int *, int);
+void pop(int *, int *);
+int stack_op(int , int);
+int main()
 {
-      int stack[SIZE], first=0, last=SIZE ,data;
-      int choice;
-     
-     do
-     {
+      int  n=0, *stk, result=0;
+    
+            printf("\nEnter number of elements:");
+            scanf("%d",&n);
+            stk = (int*) malloc(n * sizeof(int));
+            do{
+                  result = stack_op(*stk, n);
+            }while(result == 0);
+            
+            
+      return 0;
+      
+}
+int stack_op(int stk, int n)
+{
+      int choice, data, first=0, last=n-1;
+      do
+      {
             printf("\n\n\n1.push\n2.pop\n3.exit\n\n\n");
             fflush(stdin);
             scanf("%d",&choice);
             switch (choice)
             {
                   case 1:
-                        printf("input data:");
+                        printf("\ninput data (negative even numbers only):");
                         fflush(stdin);
                         scanf("%d",&data);
-                        push(stack, data, &first);
+                        if(data<0)
+                            push(&stk, data, &first, n);
+                        else  
+                        {
+                              printf("Invalid number.Re - enter!");
+                              return 0;
+                        } 
                         break;
                   
                   case 2:
-                        pop(stack, &first);
+                        pop(&stk, &first);
                         break;
 
-                  default:
-                        printf("Invalid Operation details!!");
-
-
             }
-            printf("\nstack: ");
-            for(int i=0;i<first;i++)
-                  printf("%d  ",stack[i]);
             
-     } while (choice!=3);
-     
-     
-      
-      
+      } while (choice<3);
+            
+            
+    
 }
-void push(int stack[],int data,int *f)
+void push(int stk[],int data,int *f, int n)
 {
       if(*f < 0)
             printf("\nstack underflow !!");
-      else if(*f > SIZE-1)
+      else if(*f > n-1)
             printf("\n stack is full. cannot insert !!");
       else 
       {
             printf("Data inserted successfully.\n");
-            stack[*f] = data;
+            stk[*f]= data;
             *f =  *f + 1;
       }
+      
 }
-void pop(int stack[], int *top)
+void pop(int stk[], int *top)
 {
-      if(top < 0)
+      if(*top < 0)
             printf("Stack is empty! cannot perform the operation.");
       else
       {
-            printf("Data popped.");
             *top = *top - 1;
-            printf("Data : %d",stack[*top]);
+            printf("Data popped.");
+            printf("Data : %d",stk[*top]);
       }
 }
