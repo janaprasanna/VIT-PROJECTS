94e4c0129e124e8cb6ab1f33e9dc1b836af4eed5
jana prasanna <emailtojana27@gmail.com>
2021-12-07 15:08:06 +0530

    fat lab sem 3

--- SEM3/OOPS/Practice_Concepts/files2.cpp
+++ SEM3/OOPS/Practice_Concepts/files2.cpp
@@ -5,24 +5,32 @@ using namespace std;
 // to write in a file, "<<" symbol is used.
 int main()
 {
-      ofstream file1;
+      ifstream file1;
       file1.open("write.txt");
        
        if(!file1)
        {
-             cout<<"file is not created.";
+             cout<<"file does not exists.";
        }
        else
        {
-             cout<<"file created successfully.";
-             file1<<"hello, a sample text to check write mode in files";
+             cout<<"\nfile exists.";
+             
              //file1.close();
        }
        // other method
-       char ch;
-       while((ch = getchar())!='\n')
-       {
-             file1<<ch;
-       }
-       file1.close();
+    char ch;
+    int count =0;
+    while (!file1.eof())
+    {
+        file1.get(ch);
+        if(ch=='i' || ch=='u')
+        {
+              count++;
+        }
+        //fout << ch;
+    }
+    file1.close();
+    cout<<"Number of 'i' and 'u' in the file are:"<< count;
+    return 0;
 }
